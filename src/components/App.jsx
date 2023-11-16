05bf5572248f9e4528471f3d6abf5f11ad301038
AndreeaBartic <andreeabartic20@gmail.com>
2023-11-16 09:38:42 +0200

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
+import React, { useState } from 'react';
+import Sections from './Sections';
+import FeedbackOptions from './Feedback';
+import Statistics from './Statistics';
+import Notification from './Notification';
+
 export const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = type => {
+    setState(prevState => ({ ...prevState, [type]: prevState[type] + 1 }));
+  };
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((acc, value) => acc + value, 0);
+  };
+
+  const countPositiveFeedback = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((state.good / total) * 100) : 0;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedback();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Sections title="Please leave feedback">
+        <FeedbackOptions options={state} onLeaveFeedback={handleFeedback} />
+
+        {total > 0 ? (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Sections>
     </div>
   );
 };
---
+++ src/components/Feedback.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {Object.keys(options).map(option => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      good: PropTypes.number.isRequired,
+      neutral: PropTypes.number.isRequired,
+      bad: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+export default FeedbackOptions;
